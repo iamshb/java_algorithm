23acee4467ba07c575dcb37d4db7725da165719d
iamshb <iamshb@linecorp.com>
2018-05-10 02:49:58 +0900

    add max4 method

--- src/chap01/Max3m.java
+++ src/chap01/Max3m.java
@@ -5,6 +5,7 @@ class Max3m {
         System.out.println("max3(3,2,1) = " + max3(3,2,1));
         System.out.println("max3(1,2,3) = " + max3(1,2,3));
         System.out.println("max3(1,3,2) = " + max3(1,3,2));
+        System.out.println("max4(3,2,1,4) = " + max4(3,2,1,4));
     }
 
     static int max3(int a, int b, int c) {
@@ -12,6 +13,13 @@ class Max3m {
         if (b > max) max = b;
         if (c > max) max = c;
         
+        return max;
+    }
+
+    static int max4(int a, int b, int c, int d) {
+        int max = max3(a, b, c);
+        if (d > max) max = d;
+
         return max;
     }
 }
